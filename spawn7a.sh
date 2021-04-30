d47444cd0706ee225aabd7cfd36ea11c471cf3c7
Leon Avery <pmsrw3@gmail.com>
2021-04-30 06:29:52 -0400

    Added worm7a model.

---
+++ spawn7a.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --mail-user=lavery@uwaterloo.ca
+#SBATCH --mail-type=ALL
+#SBATCH --job-name=spawn7a
+#SBATCH --output=spawn7a-%j.out
+#SBATCH --time=167:59:00
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+##SBATCH --ntasks-per-node=4
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=8G
+##SBATCH --mem=0
+set -vx
+export NCPUS=$SLURM_CPUS_PER_TASK
+export MORPHEUS=`realpath $HOME/morpheus`
+env > ${SLURM_JOB_NAME}-${SLURM_JOB_ID}.env
+cd $MORPHEUS/worm7s
+sh spawn7as.sh $SEED $STEP
---
+++ worm7/spawn7as.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -vx
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export OPENBLAS_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export MORPHEUS=`realpath $HOME/morpheus`
+export WORM7=`realpath $MORPHEUS/worm7s`
+export MODEL=worm7a.xml
+
+declare -i seed
+seed=$1
+
+step=$2
+
+if [[ $step == "new" ]]; then
+   nextsuffix="00"
+   export CHECKPOINT=worm7a${seed}last.xml
+   cp $MODEL $CHECKPOINT
+else
+   printf -v step '%02d' $step
+
+   export LASTDIR=$WORM7/sim7aseed${seed}-${step}
+   if [[ ! -f $LASTDIR/inprogress ]]; then
+       exit 0
+   fi
+   printf -v nextsuffix '%02d' $(( step + 1 ))
+   export CHECKPOINT=`ls $LASTDIR/*.xml.gz | grep -v ${seed}last.xml.gz | tail -n 1`
+fi
+export CPXML=worm7a${seed}last.xml
+export OUTDIR=$WORM7/sim7aseed${seed}-${nextsuffix}
+rm -rf $OUTDIR
+mkdir -p $OUTDIR
+if [[ $step == "new" ]]; then
+   cp $CHECKPOINT $OUTDIR/$CPXML
+   rm $CHECKPOINT
+else
+   zcat $CHECKPOINT | grep -v 'RandomSeed' > $OUTDIR/$CPXML
+fi
+gzip -v $OUTDIR/$CPXML
+echo $nextsuffix >$OUTDIR/inprogress
+export LOGOUT=$OUTDIR/worm7aseed$seed.out
+( cd $MORPHEUS; env CSVDIR=$OUTDIR sbatch -d afterburstbuffer:${SLURM_JOB_ID} csvcompress.sh )
+( cd $MORPHEUS; env SEED=$seed STEP=$nextsuffix sbatch -d afternotok:${SLURM_JOB_ID} spawn7a.sh )
+uname -a > $LOGOUT
+date >> $LOGOUT
+gtime -v morpheus -f $OUTDIR/$CPXML.gz --perf-stats --outdir $OUTDIR | timestamp >> $LOGOUT
+rm $OUTDIR/inprogress
+date >> $LOGOUT
